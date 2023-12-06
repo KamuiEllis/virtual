d3a524ea08c3ee73f85e171e75b0f77cde4d79b6
NathanEllis1 <nathane@bayshorefin.com>
2023-12-05 22:02:14 -0500

    fixes

--- database/migrations/2023_12_03_233716_create_product_table.php
+++ database/migrations/2023_12_03_233716_create_product_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sku')->unique();
+            $table->string('sku');
             $table->string('shortDescription');
             $table->longText('description');
             $table->integer('quantity');
